ee3add326b035b2540cadf08da2772d29586e0a9
Nasif Rafidi <rafidinasif117@gmail.com>
2024-05-24 10:42:35 +0600

    first commit

---
+++ First_programm.py
@@ -0,0 +1,12 @@
+print("My name is Nasif .")
+print("I am 22 years old .")
+
+print("My name is Nasif.","I am 22 years old.")
+
+print(22)
+print(18)
+
+print(22+18)
+print(22-18)
+print(22*18)
+print(22/18)
---
+++ Variables_Demo.py
@@ -0,0 +1,11 @@
+name = "Nasif" # string
+age = 22     #integer
+hight = 5.101  #double
+
+print(name)
+print(age)
+print(hight)
+
+print("My name is ",name)
+print("My age is ",age)
+print("My height is ",hight)
