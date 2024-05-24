a1c6ed672f6b56e51b5a3e0f65e9442f7a919426
Nasif Rafidi <rafidinasif117@gmail.com>
2024-05-24 19:02:52 +0600

    Variable test

---
+++ Datatype_Demo.py
@@ -0,0 +1,11 @@
+name = "Nasif" # string
+age = 22    #integer
+hight = 5.101  #Float
+old = False  #bollean
+a = None
+
+print(type(name))
+print(type(age))
+print(type(hight))
+print(type(old))
+print(type(a))
