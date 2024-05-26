207dacadfb048b04bb81a6958fba4895832e985a
Nasif Rafidi <rafidinasif117@gmail.com>
2024-05-26 12:29:51 +0600

    Else if

---
+++ Else-if_Demo.py
@@ -0,0 +1,19 @@
+light = input ("Light color :")
+
+if (light == "red"):
+ {
+    print("Stop")
+ }
+elif (light == "green"):
+    {
+        print("GO")
+    }
+    
+elif (light == "Yello"):
+    {
+        print("Ready")
+    }
+else :
+ {
+    print("Light is broken ")
+}    
