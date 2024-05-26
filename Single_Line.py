871d6aa2d84132f32b4e25c13fcfbadab3dd5831
Nasif Rafidi <rafidinasif117@gmail.com>
2024-05-26 18:04:41 +0600

    Condition statement with single line

---
+++ Single_Line.py
@@ -0,0 +1,6 @@
+food = input("Food = ")
+eat = "Yes" if food == "cake" else "No" 
+print(eat)
+
+food1 = input("Food = ")
+print("Sweet") if food == "cake" or food == "jalabi" else print("Not sweet")
