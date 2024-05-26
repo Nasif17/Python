3e9953ba5a21e7242b4736d703bb99b46e2d0c99
Nasif Rafidi <rafidinasif117@gmail.com>
2024-05-26 11:09:13 +0600

    input examples

---
+++ Input_Demo.py
@@ -0,0 +1,12 @@
+# How to take input and printing it
+
+name = input ("name = ")
+age  = int(input ("age = "))
+height  = float(input ("height = "))
+
+print(name)
+print(age)
+print(height)
+
+print ("My name is ",name,"I am ",age,"years old ")
+print("My height is ",height)
