1fa03dcc04215f4229ee13376b951ea9f910941e
Nasif Rafidi <rafidinasif117@gmail.com>
2024-05-26 12:50:45 +0600

    Mark distribution

---
+++ Marks_distribution.py
@@ -0,0 +1,29 @@
+marks =int (input("Marks = "))
+if(marks>=90):
+    {
+        print("A+")
+    }
+elif(marks<90 and marks>=85):
+    {
+        print("A")
+    }
+elif(marks<85 and marks>=80):
+    {
+        print("B+")
+    }
+elif(marks<80 and marks>=75):
+    {
+        print("B")
+    }
+elif(marks<75 and marks>=70):
+    {
+        print("C+")
+    }
+elif(marks<90 and marks>=85):
+    {
+        print("D")
+    }
+else:
+    {
+        print("Failed")
+    }
