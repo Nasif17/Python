53f9a4b7f221d4b8bdd0cd916dda4f9a475b6040
Nasif Rafidi <rafidinasif117@gmail.com>
2024-07-30 11:19:55 +0600

    Logical operators

---
+++ logical_operators.py
@@ -0,0 +1,13 @@
+a = 50
+b = 20
+
+print(not False)
+print (not (a>b))
+
+vul1 = True
+vul2 = True
+print ("Assignment operators : ", vul1 and vul2)
+
+vul3 = True
+vul4 = False
+print ("Assignment operators : ", vul3 and vul4)
