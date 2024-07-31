0c8e76c5ddbd72d652f5ac396697ef2d471635c3
Nasif Rafidi <rafidinasif117@gmail.com>
2024-07-31 12:36:28 +0600

    practice

---
+++ input_practice.py
@@ -0,0 +1,8 @@
+name = input ("Enter your name : ")
+age = int(input("Enter your age : "))
+marks = float(input("Enter your marks :"))
+
+
+print("Welcome : ",name)
+print("age : ",age)
+print("marks : ",marks)
