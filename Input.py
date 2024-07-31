7cd6679b16df9f96423200f4793724a39e451ed2
Nasif Rafidi <rafidinasif117@gmail.com>
2024-07-31 12:05:44 +0600

    Find input type

---
+++ Input.py
@@ -0,0 +1,8 @@
+name = input ("Enter your name : ")
+print("Welcome ", name)
+
+val = input("enter some number")
+print(type(val),val) 
+
+val1 = int (input("enter some number"))
+print(type(val1),val1) 
