11aae55a13b7542bdcce9e0d18d96cab3c4afa78
Nasif Rafidi <rafidinasif117@gmail.com>
2024-08-03 10:17:28 +0600

    Sliching thriugh string

---
+++ Sliching.py
@@ -0,0 +1,12 @@
+str = "Nasif Rafidi"
+
+print(str[1:4])  
+
+
+"""Sliching use for machine learning
+it's used in string to show a slich part of 
+your object"""
+
+print (str[6:len(str)])
+
+print (str[6:])
