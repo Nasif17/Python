60d02f395700118ccdae73bcadd15e5b45d574fe
Nasif Rafidi <rafidinasif117@gmail.com>
2024-08-13 22:34:31 +0600

    list initializing and manupulatin

---
+++ List.py
@@ -0,0 +1,16 @@
+marks = [94.4,87.5,95.2,66.4,45.1]
+print (marks)
+print (type(marks))
+print (marks[0])
+print (marks[1])
+print (len(marks))
+
+"""A single List can store multiple types of data"""
+
+#For Example
+
+student = ["Karan",95.4,17,"Delhi"]
+print(student)
+
+student[0] = "arjun"
+print(student)
