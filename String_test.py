912e11bb5810525056aed72537365f58e123eea5
Nasif Rafidi <rafidinasif117@gmail.com>
2024-08-13 09:15:11 +0600

    String test

---
+++ String_test.py
@@ -0,0 +1,8 @@
+#write a programme to input user's first name & print its length
+str = input("Enter your first name : ")
+print(str)
+print(len(str))
+
+#write a programme to find the occurrence of '$'in string.
+print(str.find("$"))
+print(str.count("$"))
