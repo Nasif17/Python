17534b625c64de7a92b671607d0af2acf7aac536
Nasif Rafidi <rafidinasif117@gmail.com>
2024-08-24 10:56:12 +0600

    Tuples

---
+++ Tuples.py
@@ -0,0 +1,30 @@
+tup = (2,1,3,1)
+print(type(tup))   #tupple is immutable
+
+print(tup[0])
+print(tup[1])
+
+tup1 = ()
+print (tup1)
+print(type(tup))
+
+tup2 = (1)
+print(tup2)
+print(type(tup2))
+
+tup3 = (1,)
+print(tup3)
+print(type(tup3))
+
+
+tup4 = (1,2,3,4,)
+print(tup4)
+print(type(tup4))
+
+tup5 =("Hello")
+print(tup5)
+print(type(tup5))
+
+tup6 = ("Hello",)
+print(tup6)
+print(type(tup6))
