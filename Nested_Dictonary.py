9f527b6731b49dc5df96738484a227bab886dceb
Nasif Rafidi <rafidinasif117@gmail.com>
2024-08-28 11:36:49 +0600

    Nested Dictonry

---
+++ Nested_Dictonary.py
@@ -0,0 +1,13 @@
+student = {
+    "name" : "Nasif",
+    "score" : {
+        "chem": 98,
+        "phy": 97,
+        "math":95
+    }
+}
+
+# nested dictyonary
+print(student)
+print(student["score"])
+print(student["score"]["chem"])
