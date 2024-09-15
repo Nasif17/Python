f638bf1436deeb2b18d02db918d3ed9c652c6593
Nasif Rafidi <rafidinasif117@gmail.com>
2024-09-15 12:55:58 +0600

    Set

---
+++ Set.py
@@ -0,0 +1,19 @@
+collection = {1,2,3,4,5,"hello","world"}
+print(collection)
+print(type(collection))
+print(len(collection))  #total number of item
+
+
+collection = {1,2,2,3,3,4,5,"hello","world","world","hello"}
+print(collection)
+print(type(collection))
+print(len(collection))   #total number item
+
+
+collection = {}   # empty dictonary
+print(collection)
+print(type(collection))
+
+collection = set()   # empty set; syntax
+print(collection)
+print(type(collection))
