223e7c30056724919c20e6202dfe13eb9bf1433d
Nasif Rafidi <rafidinasif117@gmail.com>
2024-09-16 20:43:28 +0600

    set and dictonary test

---
+++ Set_Dictonary_Test.py
@@ -0,0 +1,24 @@
+"""Store following word meanings in ptthon dictonary
+table : "a piece of furniture" , "list of facts & figures"
+cat  : "a small animal" """
+
+dictionary = {
+    "cat" : "a samll animal",
+    "table" : ["a piece of furniture","list of facts & figures"]
+    
+}
+print (dictionary)
+
+"""you are given a list subject for students. Assume one classroom is rerquiredd for
+1 subject. How many Classrooms are needed by all students.
+
+"python","java","C++","python","javascript",
+"java","python","java","C++","C"
+
+"""
+
+set = {"python","java","C++","python","javascript",
+"java","python","java","C++","C"}
+
+print(len(set))
+
