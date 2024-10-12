56149ac0f9969d07d1263ac6f5cf64bfc0780cd5
Nasif Rafidi <rafidinasif117@gmail.com>
2024-10-12 09:32:49 +0600

    While loop basic

---
+++ While_Loop.py
@@ -0,0 +1,31 @@
+count = 0
+
+while count <= 5 :
+    print ("hello")
+    count += 1 
+    
+print (count)    
+
+
+count = 0
+
+while count < 5 :
+    print ("hello")
+    count += 1 
+    print(count)
+    
+count = 0
+
+while count <= 5 :
+    print ("hello",count)
+    count += 1 
+    
+print (count) 
+
+# reverse loop
+i = 5
+while i>=1 :
+    print(i)
+    i -=1
+    
+print ("Loop ended")    
