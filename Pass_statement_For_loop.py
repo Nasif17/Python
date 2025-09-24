6923318a9b6ddf88512a01dd3a5a73416c930f05
Your Name <rafidinasif117@gmail.com>
2025-09-24 21:17:12 +0600

    Pass Statement

---
+++ Pass_statement_For_loop.py
@@ -0,0 +1,6 @@
+for i in range(5):
+    pass
+    print("Hello, World!")
+    
+"""This is a simple example of using the pass statement in a for loop.
+The pass statement is a null operation; it does nothing when executed."""
